662e79a30fe4546ca07411ece3ed7edc648bb2d1
bulqistore <bulqistore@gmail.com>
2026-01-17 12:52:03 -0500

    Improve camera scanner UX: full-width mobile popup, better barcode detection

--- resources/ts/popups/ns-pos-camera-scan.vue
+++ resources/ts/popups/ns-pos-camera-scan.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="camera-scan-popup" class="ns-box shadow-lg w-95vw h-95vh md:h-[60vh] md:w-half flex flex-col overflow-hidden">
+    <div id="camera-scan-popup" class="ns-box shadow-lg w-full h-95vh md:h-[70vh] md:w-3/4 lg:w-2/3 flex flex-col overflow-hidden">
         <div class="p-2 border-b ns-box-header flex justify-between items-center">
             <h3 class="text-fontcolor">{{ __( 'Scan Product' ) }}</h3>
             <div>
@@ -21,7 +21,7 @@
 </template>
 
 <script>
-import { Html5Qrcode } from "html5-qrcode";
+import { Html5Qrcode, Html5QrcodeSupportedFormats } from "html5-qrcode";
 import popupCloser from "~/libraries/popup-closer";
 import popupResolver from '~/libraries/popup-resolver';
 import { nsHttpClient, nsSnackBar } from '~/bootstrap';
@@ -72,11 +72,29 @@ export default {
         },
 
         startScanning(cameraId) {
+            // Calculate optimal qrbox size based on viewport
+            const viewportWidth = Math.min(window.innerWidth, 500);
+            const qrboxSize = Math.floor(viewportWidth * 0.8);
+            
             this.html5QrCode.start(
                 cameraId, 
                 {
-                    fps: 10,
-                    qrbox: { width: 250, height: 250 }
+                    fps: 20, // Increased FPS for smoother scanning
+                    qrbox: { 
+                        width: qrboxSize, 
+                        height: Math.floor(qrboxSize * 0.5) // Wider box for barcodes
+                    },
+                    aspectRatio: 1.777778, // 16:9 aspect ratio
+                    // Prioritize barcode formats
+                    formatsToSupport: [
+                        Html5QrcodeSupportedFormats.EAN_13,
+                        Html5QrcodeSupportedFormats.EAN_8,
+                        Html5QrcodeSupportedFormats.CODE_128,
+                        Html5QrcodeSupportedFormats.CODE_39,
+                        Html5QrcodeSupportedFormats.UPC_A,
+                        Html5QrcodeSupportedFormats.UPC_E,
+                        Html5QrcodeSupportedFormats.QR_CODE
+                    ]
                 },
                 (decodedText, decodedResult) => {
                     this.onScanSuccess(decodedText, decodedResult);
